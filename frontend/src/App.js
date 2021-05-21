f4c5ddc0402239ab195e9774716943de40f8b3c0
swagatikapanda123 <swagatikapanda164@gmail.com>
2021-05-21 12:59:22 +0530

    added models

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router , Route} from 'react-router-dom'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <div className="App">
-      <h1>pro shop</h1>
-    </div>
-  );
+    <Router>
+      <Header />
+      <main className='py-3'> 
+        <Container>
+          < Route path='/' component={ HomeScreen } exact />
+          < Route path='/product/:id' component={ ProductScreen } />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>PageGolden</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='justify-content-end'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  Cart <i className='fas fa-shopping-cart'></i>
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  Sign In <i className='fas fa-user'></i>
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
