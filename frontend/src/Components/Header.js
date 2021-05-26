69445da8e5b26e0e127f1b3639fad387f0a63d3a
swagatikapanda123 <swagatikapanda164@gmail.com>
2021-05-27 01:08:39 +0530

    logout

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
+  const dispatch = useDispatch()
+  const userLogin = useSelector(state => state.userLogin )
+  const { userInfo } = userLogin
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -18,11 +26,21 @@ const Header = () => {
                   Cart <i className='fas fa-shopping-cart'></i>
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  Sign In <i className='fas fa-user'></i>
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ): <LinkContainer to='/login'>
+              <Nav.Link>
+                Sign In <i className='fas fa-user'></i>
+              </Nav.Link>
+            </LinkContainer>}
+              
             </Nav>
           </Navbar.Collapse>
         </Container>
