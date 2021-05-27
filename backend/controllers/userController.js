b00e9455815c585834d51433474c7f030b69e5e9
swagatikapanda123 <swagatikapanda164@gmail.com>
2021-05-27 21:48:36 +0530

    update profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,7 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email })
   if (user && (await user.matchPassword(password))) {
     res.json({
-      id: user._id,
+      _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
@@ -24,27 +24,29 @@ const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
 
   const userExists = await User.findOne({ email })
-  
-  if(userExists){
-      res.status(400)
-      throw new Error ('User already exists')
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
   }
 
   const user = await User.create({
-      name, email, password
+    name,
+    email,
+    password,
   })
 
-  if(user) {
-      res.status(201).json({
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user._id),
-      })
-  } else{
-      res.status(400)
-      throw new Error ('Invalid user data')
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
   }
 })
 
@@ -64,4 +66,30 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile, registerUser }
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    ;(user.name = req.body.name || user.name),
+      (user.email = req.body.email || user.email)
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name:updatedUserr.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    })
+
+  } else {
+    res.status(401)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile, registerUser, updateUserProfile }
