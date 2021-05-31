2541c84cf572ce632e1d5fcf30c8fa673166cfba
swagatikapanda123 <swagatikapanda164@gmail.com>
2021-06-01 02:09:02 +0530

    paypal payment added

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 
-
 dotenv.config()
 
 connectDB()
@@ -22,6 +21,10 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
 app.use(notFound)
 
