323aae406158f26e7fd99be9e9a2f1685fc80389
swagatikapanda123 <swagatikapanda164@gmail.com>
2021-05-31 16:07:08 +0530

    getOrderById

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -12,27 +12,39 @@ const addOrderItems = asyncHandler(async (req, res) => {
     totalPrice,
   } = req.body
 
-  if(orderItems && orderItems.length == 0){
-      res.status(400)
-      throw new Error('No order Items')
-      return
+  if (orderItems && orderItems.length == 0) {
+    res.status(400)
+    throw new Error('No order Items')
+    return
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    })
+
+    const createdOrder = await order.save()
+
+    res.status(201).json(createdOrder)
   }
-  else{
-      const order = new Order({
-        orderItems,
-        user: req.user._id,
-        shippingAddress,
-        paymentMethod,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-      })
-
-      const createdOrder = await order.save()
-
-      res.status(201).json(createdOrder)
-    }
 })
 
-export { addOrderItems }
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+  if(order) {
+    res.json(order)
+  } else{
+    res.status(404)
+    throw new Error ('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById }
