49013326869fdf8f3b86d78452c4571ddeb3b153
swagatikapanda123 <swagatikapanda164@gmail.com>
2021-06-05 01:05:52 +0530

    git admin userlist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
+import UserListScreen from './screens/UserListScreen'
 
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
           < Route path='/payment' component={ PaymentScreen } />
           < Route path='/placeorder' component={ PlaceOrderScreen } />
           < Route path='/order/:id' component={ OrderScreen } />
+          < Route path='/admin/userlist' component={ UserListScreen } />
         </Container>
       </main>
       <Footer />
