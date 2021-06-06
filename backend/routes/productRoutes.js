c2be35f531f7b55f19d8f33dffb4c65e8b6b466e
swagatikapanda123 <swagatikapanda164@gmail.com>
2021-06-06 20:56:12 +0530

    admin delete product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import {
+  deleteProduct,
   getProductById,
   getProducts,
 } from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
+
+
 const router = express.Router()
 
-//fetch all products
 router.route('/').get(getProducts)
 
-//fetch single product
-router.route('/:id').get(getProductById)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
 
 export default router
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
-
+import ProductListScreen from './screens/ProductListScreen'
 
 
 function App() {
@@ -35,6 +35,7 @@ function App() {
           < Route path='/placeorder' component={ PlaceOrderScreen } />
           < Route path='/order/:id' component={ OrderScreen } />
           < Route path='/admin/userlist' component={ UserListScreen } />
+          < Route path='/admin/productlist' component={ ProductListScreen } />
           < Route path='/admin/user/:id/edit' component={ UserEditScreen } />
         </Container>
       </main>
