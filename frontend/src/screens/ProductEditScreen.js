7c46deb908151fe7bfc7b5ebdbc3a6920ac66a35
swagatikapanda123 <swagatikapanda164@gmail.com>
2021-06-09 00:20:33 +0530

    image upload config

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -4,8 +4,9 @@ import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../Components/Message'
 import Loader from '../Components/Loader'
-import { listProductDetails } from '../actions/productActions'
+import { listProductDetails, updateProduct } from '../actions/productActions'
 import FormContainer from '../Components/FormContainer'
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstant'
 
 const ProductEditScreen = ({ match, history }) => {
   const productId = match.params.id
@@ -23,23 +24,42 @@ const ProductEditScreen = ({ match, history }) => {
   const productDetails = useSelector((state) => state.productDetails)
   const { loading, error, product } = productDetails
 
+  const productUpdate = useSelector((state) => state.productUpdate)
+  const { loading: loadingUpdate, error: errorUpdate , success: successUpdate } = productUpdate
+
+
   useEffect(() => {
-    if (!product.name || product._id !== productId) {
-      dispatch(listProductDetails(productId))
-    } else {
-      setName(product.name)
-      setPrice(product.price)
-      setImage(product.image)
-      setBrand(product.brand)
-      setCategory(product.category)
-      setCountInStock(product.countInStock)
-      setDescription(product.description)
-    }
-  }, [dispatch, productId, product, history])
+      if(successUpdate) {
+          dispatch({ type: PRODUCT_UPDATE_RESET })
+          history.push('/admin/productlist')
+      } else{
+        if (!product.name || product._id !== productId) {
+            dispatch(listProductDetails(productId))
+          } else {
+            setName(product.name)
+            setPrice(product.price)
+            setImage(product.image)
+            setBrand(product.brand)
+            setCategory(product.category)
+            setCountInStock(product.countInStock)
+            setDescription(product.description)
+          }
+      }
+    
+  }, [dispatch, productId, product, history, successUpdate])
 
   const submitHandler = (e) => {
     e.preventDefault()
-    //update product
+    dispatch(updateProduct({
+        _id: productId,
+        name,
+        price,
+        image,
+        brand,
+        category,
+        description,
+        countInStock
+    }))
   }
 
   return (
@@ -50,6 +70,8 @@ const ProductEditScreen = ({ match, history }) => {
 
       <FormContainer>
         <h1>Update Product</h1>
+        {loadingUpdate && <Loader /> }
+        {errorUpdate && <Message variant='danger'> {errorUpdate} </Message>}
         {loading ? (
           <Loader />
         ) : error ? (
